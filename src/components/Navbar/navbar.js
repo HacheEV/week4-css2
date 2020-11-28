dc07d7f99ea465a5bfce92f2458fa8d37c692fb0
Hector <hector.evalverde@gmail.com>
2020-11-28 14:28:22 +0100

    second button display

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,6 +2,7 @@ import './navbar.css';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () =>{
     
@@ -21,7 +22,8 @@ const Navbar = () =>{
                 <li className={`item_${active ? "show" : "hide"}`}><a  href="http://localhost:3000/">CSS</a></li>
                 <li className={`item_${active ? "show" : "hide"}`}><a  href="http://localhost:3000/">CSS Layout</a></li>
                 <li className={`item_${active ? "show" : "hide"}`}><a  href="http://localhost:3000/">CSS Layout</a></li>
-                <li className={`button-${active ? "hide" : "show"}`} onClick={handleClick}><FontAwesomeIcon icon={faBars}/></li>
+                <li className={`button-${active ? "show" : "hide"}`} onClick={handleClick}><FontAwesomeIcon icon={faBars}/></li>
+                <li className={`button-${active ? "hide" : "show"}`} onClick={handleClick}><FontAwesomeIcon icon={faTimes}/></li>
             </ul>
         </div>
 
